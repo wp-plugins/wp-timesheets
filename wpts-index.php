8138cdd98a563d48a9e542a007bd246d2b8ac8c4
akshay_raje <akshay_raje@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-29 18:46:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-timesheets/tags/0.1@159059 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpts-index.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: WP TimeSheets
+Plugin URI: http://webdlabs.com/projects/wp-timesheets/
+Description: A simple timesheet software for WordPress
+Author: Akshay Raje
+Version: 0.1
+Author URI: http://webdlabs.com
+*/
+
+add_action('admin_menu', 'wpts_setup');
+register_activation_hook(__FILE__,'wpts_install');
+
+function wpts_install() {
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'timesheets';
+	if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+		$sql = "CREATE TABLE " . $table_name . " (
+		  `ID` bigint(20) unsigned NOT NULL auto_increment,
+		  `ts_author` bigint(20) NOT NULL,
+		  `ts_date` date NOT NULL,
+		  `ts_job_name` text NOT NULL,
+		  `ts_description` text NOT NULL,
+		  `ts_time_in` time NOT NULL,
+		  `ts_time_out` time NOT NULL,
+		  `ts_hours` time NOT NULL,
+		  PRIMARY KEY  (`ID`)
+		) ENGINE=MyISAM  DEFAULT CHARSET=utf8;";
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+		$welcome_name = "Mr. Wordpress";
+		$welcome_text = "Congratulations, you just completed the installation!";
+	}
+}
+
+function wpts_setup() {
+  $wpts_home = add_menu_page('TimeSheets', 'TimeSheets', 1, 'wpts-ts-new.php', 'wpts_view_add_page');
+  $wpts_view_add = add_submenu_page('wpts-ts-new.php', 'Manage', 'Manage', 1, 'wpts-ts-new.php', 'wpts_view_add_page');
+  $wpts_reports = add_submenu_page('wpts-ts-new.php', 'Reports', 'Reports', 1, 'wpts-reports.php', 'wpts_reports_page'); 
+  add_action('admin_head-'.$wpts_home, 'wpts_admin_header' );
+  add_action('admin_head-'.$wpts_view_add, 'wpts_admin_header' );
+}
+
+function wpts_admin_header() {
+	if($_REQUEST['doaction'] == 'Add' || $_REQUEST['doaction'] == 'Edit') {
+		$url = get_settings('siteurl');
+		$url = $url . '/wp-content/plugins/wp-timesheets/';
+		echo '<link rel="stylesheet" type="text/css" media="all" href="'.$url.'timePicker.css" />'."\n";
+		echo '<script type="text/javascript" src="'.$url.'jquery.timePicker.js"></script>'."\n";
+		echo '<script type="text/javascript" src="'.$url.'wpts-jslib.js"></script>'."\n";
+	}
+}
+
+function wpts_view_add_page() {
+	require_once('wpts-ts-new.php');
+}
+
+function wpts_reports_page() {
+	require_once('wpts-reports.php');
+}
+?>
---
+++ wpts-jslib.js
@@ -0,0 +1,52 @@
+jQuery(function() {
+
+	jQuery("#ts_time_in, #ts_time_out").timePicker({
+		step: 15,
+		show24Hours: false
+	});	
+	// Store time used by duration.
+	
+	var oldTime = jQuery.timePicker("#ts_time_in").getTime();
+	var duration = (jQuery.timePicker("#ts_time_out").getTime() - oldTime);
+	jQuery("#ts_hours_display").val(time_diff(duration));
+	jQuery("#ts_hours").val(time_diff(duration));
+		
+	// Keep the duration between the two inputs.
+	jQuery("#ts_time_in").change(function() {
+		if (jQuery("#ts_time_out").val()) { // Only update when second input has a value.
+			// Calculate duration.
+			var duration = (jQuery.timePicker("#ts_time_out").getTime() - oldTime);
+			var time = jQuery.timePicker("#ts_time_in").getTime();	
+			// Calculate and update the time in the second input.
+			jQuery.timePicker("#ts_time_out").setTime(new Date(new Date(time.getTime() + duration)));
+			jQuery("#ts_hours_display").val(time_diff(duration));
+			jQuery("#ts_hours").val(time_diff(duration));
+			oldTime = time;
+		}
+	});
+	
+	// Validate.
+	jQuery("#ts_time_out").change(function() {
+		// Store time used by duration.
+		var oldTime = jQuery.timePicker("#ts_time_in").getTime();
+		var duration = (jQuery.timePicker("#ts_time_out").getTime() - oldTime);
+			if(jQuery.timePicker("#ts_time_in").getTime() > jQuery.timePicker(this).getTime()) {
+				jQuery(this).addClass("error");
+			} else {
+				jQuery(this).removeClass("error");
+			}
+		jQuery("#ts_hours_display").val(time_diff(duration));
+		jQuery("#ts_hours").val(time_diff(duration));
+	});
+
+});
+
+function time_diff(duration){
+	// Calculate formated timediff
+	timediff = duration;
+	hours = Math.floor(timediff / (1000 * 60 * 60)); 
+	timediff -= hours * (1000 * 60 * 60);
+	mins = Math.floor(timediff / (1000 * 60)); 
+	timediff -= mins * (1000 * 60);
+	return hours+':'+mins;
+}
