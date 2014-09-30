72479655e17426b5df7fa3c5a43aa993b1de838e
akshay_raje <akshay_raje@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 07:50:55 +0000

    1.0: Complete rewrite. custom hour input fields, admin customization; security Fixes
    git-svn-id: https://plugins.svn.wordpress.org/wp-timesheets/tags/1.0@998958 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/other-fields.php
@@ -0,0 +1,13 @@
+			<tr>
+				<th scope="row"><?=_e('Time spent in', 'wp_timesheets' )?></th>
+				<td>
+<?php foreach($data as $field) : 
+	if($view === false): ?>
+					<p><?=$field?> <input name="other_field[<?=$field?>]" type="number" step="0.5" id="other_field[<?=$field?>]" class="small-text" value="<?=$values[$field]?>" /> <?=_e('Hours', 'wp_timesheets' )?></p>
+<?php elseif($view === true) : 
+	if( empty($values[$field]) ) $values[$field] = '0'; ?>
+					<p><?=$field?> <?=$values[$field]?> <?=_e('Hours', 'wp_timesheets' )?></p>
+<?php endif; endforeach; ?>
+				</td>
+			</tr>
+
